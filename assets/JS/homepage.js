a5e9cbba1a75f017d367bfb22c31e6932a17e4db
Kona <landon.deroin@gmail.com>
2023-07-17 13:58:37 -0400

    youtube api implemented and displaying video

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -32,3 +32,44 @@ $(document).ready(function () {
     alert("Button clicked!");
   });
 });
+ 
+
+var apiKey = 'AIzaSyBAxCtGMC0LTqmTteYtwiNgPO_uQxXRexE';
+var apiUrl = 'https://www.googleapis.com/youtube/v3/search';
+
+fetch(apiUrl + '?part=snippet&q=chest workout&type=video&maxResults=10&key=' + apiKey)
+  .then(function(response) {
+    if (response.ok) {
+      return response.json();
+      
+    }
+    throw new Error('Request failed. Status code: ' + response.status);
+  })
+  .then(function(data) {
+    console.log(data);
+    var videoId = data.items[0].id.videoId;
+    var videoTitle = data.items[0].snippet.title;
+    var videoThumbnail = data.items[0].snippet.thumbnails.default.url;
+
+    // Create an iframe element for the video
+    var iframe = document.createElement('iframe');
+    iframe.src = 'https://www.youtube.com/embed/' + videoId;
+    iframe.title = videoTitle;
+
+    // Create an image element for the video thumbnail
+    var thumbnail = document.createElement('img');
+    thumbnail.src = videoThumbnail;
+    thumbnail.alt = videoTitle;
+
+    // Create a container element for the video
+    var videoContainer = document.createElement('div');
+    videoContainer.classList.add('video-item');
+    videoContainer.appendChild(iframe);
+    
+
+    // Append the video container to the main container in your HTML
+    document.getElementById('videoContainer').appendChild(videoContainer);
+  })
+  .catch(function(error) {
+    console.log('Error:', error.message);
+  });
