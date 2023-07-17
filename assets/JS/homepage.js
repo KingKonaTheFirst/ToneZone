5c3ebce6e5c7580373beefe87775d3d60bffe0c6
Kona <landon.deroin@gmail.com>
2023-07-17 11:13:17 -0400

    added click listeners to sidebar buttons

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -4,3 +4,31 @@ var shoulders = $("#shoulders");
 var legs = $("#legs");
 var abs = $("#abs");
 var cardio = $("#cardio");
+
+$(document).ready(function () {
+  // Add click listener to the button
+  $("#chest").click(function () {
+    // Code to execute when the button is clicked
+    alert("Button clicked!");
+  });
+  $("#back").click(function () {
+    //   alert for testing delete later
+    alert("Button clicked!");
+  });
+  $("#shoulders").click(function () {
+    //   alert for testing delete later
+    alert("Button clicked!");
+  });
+  $("#legs").click(function () {
+    //   alert for testing delete later
+    alert("Button clicked!");
+  });
+  $("#abs").click(function () {
+    //   alert for testing delete later
+    alert("Button clicked!");
+  });
+  $("#cardio").click(function () {
+    //   alert for testing delete later
+    alert("Button clicked!");
+  });
+});
